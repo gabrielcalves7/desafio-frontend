d1bdafbae3221fecb6ad3844a44a26834974a51f
gabriel alves <gabriel@webtagger.com.br>
2022-05-29 12:14:26 -0300

    Ajuste na API

--- pokemons/src/API/pokemonInfo.jsx
+++ pokemons/src/API/pokemonInfo.jsx
@@ -1,6 +1,5 @@
-const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
-
 export async function fetchPokemonData(){
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
   return fetch(`${url}`)
   .then( response => response.json())
   .then(response => response.results)
